2c375fdc3d6de271e3c77703c4f1d125637868a1
Dival Sehgal <sehgaldival@gmail.com>
2024-01-04 23:48:03 +0530

    web question added

--- generators/index.js
+++ generators/index.js
@@ -13,3 +13,16 @@ console.log(idGenerator.next().value); // 1
 console.log(idGenerator.next().value); // 2
 console.log(idGenerator.next().value); // 3
 
+
+// function* mygenerator(intitalValue=0){
+//   let id = intitalValue;;
+//   while(true) {
+//     yield id++
+//   }
+// }
+// const temp = mygenerator(4)
+
+
+// console.log(temp.next().value); //
+
+// console.log(temp.next().value); //
