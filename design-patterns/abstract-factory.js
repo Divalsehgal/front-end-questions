3c91a1b7d4954b4b0b00f1ee84f71fe8cf026a32
Dival Sehgal <sehgaldival@gmail.com>
2026-03-31 12:14:25 +0530

    feat: implement design patterns and add Redux documentation

---
+++ design-patterns/abstract-factory.js
@@ -0,0 +1,55 @@
+/*
+Abstract Factory pattern allows us to produce related objects
+without specifying concrete classes/functions.
+
+It's useful in situations where we need to create objects
+that share only some properties and methods
+Basically Adding an abstraction layer over the factory method pattern
+*/
+
+
+// We have a class or "concrete factory" for each vehicle type
+class Car {
+    constructor() {
+        this.name = "Car"
+        this.wheels = 4
+    }
+    turnOn = () => console.log("Chacabúm!!")
+}
+
+class Truck {
+    constructor() {
+        this.name = "Truck"
+        this.wheels = 8
+    }
+    turnOn = () => console.log("RRRRRRRRUUUUUUUUUMMMMMMMMMM!!")
+}
+
+class Motorcycle {
+    constructor() {
+        this.name = "Motorcycle"
+        this.wheels = 2
+    }
+    turnOn = () => console.log("sssssssssssssssssssssssssssssshhhhhhhhhhham!!")
+}
+
+// And and abstract factory that works as a single point of interaction for our clients
+// Given the type parameter it receives, it will call the corresponding concrete factory
+const vehicleFactory = {
+    createVehicle: function (type) {
+        switch (type) {
+            case "car":
+                return new Car()
+            case "truck":
+                return new Truck()
+            case "motorcycle":
+                return new Motorcycle()
+            default:
+                return null
+        }
+    }
+}
+
+const car = vehicleFactory.createVehicle("car") // Car { turnOn: [Function: turnOn], name: 'Car', wheels: 4 }
+const truck = vehicleFactory.createVehicle("truck") // Truck { turnOn: [Function: turnOn], name: 'Truck', wheels: 8 }
+const motorcycle = vehicleFactory.createVehicle("motorcycle") // Motorcycle { turnOn: [Function: turnOn], name: 'Motorcycle', wheels: 2 }
